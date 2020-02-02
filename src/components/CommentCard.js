a150e0f2c91d24c6056c6d813a61830d84dc8049
Jordan <jordandoan@hotmail.com>
2020-02-01 21:30:40 -0800

    Setting up comment card

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './CommentCard.module.scss';
 
 const CommentCard = ({comment}) => {
+  const [showReply, setReply] = useState(false)
   console.log(comment)
   return (
     <div className={styles.comment}>
-      {comment.text}  posted by {comment.user.name}
+      {comment.text}  
+      <div className={styles.byline}>
+        <p>posted by {comment.user.name}</p>
+        <p onClick={() => setReply(!showReply)}>{showReply ? "Cancel" : "Reply"}</p>
+      </div>
+      {showReply && <div>Type your response here</div>}
       {comment.replies && comment.replies.map(reply => <CommentCard comment={reply}/>)}
     </div>
   )
